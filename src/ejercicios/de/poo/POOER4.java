6353456d2f6fe142a632d26d5a6ed67dad08bcf9
Sebastián <Sebastin@DESKTOP-OFT45F4>
2023-08-30 22:24:41 -0500

    Ejercicio resuelto #4

---
+++ src/ejercicios/de/poo/POOER4.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicios.de.poo;
+
+/**
+ *
+ * @author Sebastián
+ */
+public class POOER4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Ejercicio resuelto #4
+        int EDJUAN=9;
+        int EDALBER= 2*EDJUAN/3;
+        int EDANA= 4*EDJUAN/3;
+        int EDMAMA= EDJUAN + EDALBER + EDANA;
+        System.out.println("Las edades son: ");
+        System.out.println("Alberto: " + EDALBER);
+        System.out.println("Juan: " + EDJUAN);
+        System.out.println("Ana: " + EDANA);
+        System.out.println("Mama: " + EDMAMA);
+
+    }
+    
+}
